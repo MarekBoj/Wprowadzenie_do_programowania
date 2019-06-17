63a8372b00c1685bee5f759ec8a90b00ddb78732
MarekBoj <48556256+MarekBoj@users.noreply.github.com>
2019-06-17 11:51:07 +0200

    Add files via upload

---
+++ Projekt Wprowadzenie do programowania - Marek Bojarski 11529/Pong Game/Pong.Designer.cs
@@ -0,0 +1,156 @@
+﻿namespace Pong_Game
+{
+    partial class PongWindow
+    {
+        /// <summary>
+        /// Wymagana zmienna projektanta.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Wyczyść wszystkie używane zasoby.
+        /// </summary>
+        /// <param name="disposing">prawda, jeżeli zarządzane zasoby powinny zostać zlikwidowane; Fałsz w przeciwnym wypadku.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kod generowany przez Projektanta formularzy systemu Windows
+
+        /// <summary>
+        /// Metoda wymagana do obsługi projektanta — nie należy modyfikować
+        /// jej zawartości w edytorze kodu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(PongWindow));
+            this.Left_player_point = new System.Windows.Forms.TextBox();
+            this.Right_player_point = new System.Windows.Forms.TextBox();
+            this.Timer = new System.Windows.Forms.Timer(this.components);
+            this.Platform_left = new System.Windows.Forms.PictureBox();
+            this.Platform_right = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.Ball = new System.Windows.Forms.PictureBox();
+            this.label1_pause = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.Platform_left)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Platform_right)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Ball)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // Left_player_point
+            // 
+            this.Left_player_point.AutoCompleteCustomSource.AddRange(new string[] {
+            resources.GetString("Left_player_point.AutoCompleteCustomSource")});
+            this.Left_player_point.BackColor = System.Drawing.Color.Snow;
+            this.Left_player_point.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.Left_player_point.Cursor = System.Windows.Forms.Cursors.Default;
+            resources.ApplyResources(this.Left_player_point, "Left_player_point");
+            this.Left_player_point.ForeColor = System.Drawing.Color.Black;
+            this.Left_player_point.Name = "Left_player_point";
+            this.Left_player_point.ReadOnly = true;
+            // 
+            // Right_player_point
+            // 
+            this.Right_player_point.AutoCompleteCustomSource.AddRange(new string[] {
+            resources.GetString("Right_player_point.AutoCompleteCustomSource")});
+            this.Right_player_point.BackColor = System.Drawing.Color.Snow;
+            this.Right_player_point.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.Right_player_point.Cursor = System.Windows.Forms.Cursors.Default;
+            resources.ApplyResources(this.Right_player_point, "Right_player_point");
+            this.Right_player_point.ForeColor = System.Drawing.Color.Black;
+            this.Right_player_point.Name = "Right_player_point";
+            this.Right_player_point.ReadOnly = true;
+            // 
+            // Timer
+            // 
+            this.Timer.Enabled = true;
+            this.Timer.Interval = 2;
+            this.Timer.Tick += new System.EventHandler(this.Timer_Tick);
+            // 
+            // Platform_left
+            // 
+            this.Platform_left.BackColor = System.Drawing.Color.White;
+            this.Platform_left.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            resources.ApplyResources(this.Platform_left, "Platform_left");
+            this.Platform_left.Name = "Platform_left";
+            this.Platform_left.TabStop = false;
+            // 
+            // Platform_right
+            // 
+            this.Platform_right.BackColor = System.Drawing.Color.White;
+            this.Platform_right.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            resources.ApplyResources(this.Platform_right, "Platform_right");
+            this.Platform_right.Name = "Platform_right";
+            this.Platform_right.TabStop = false;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.White;
+            this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            resources.ApplyResources(this.pictureBox1, "pictureBox1");
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.TabStop = false;
+            // 
+            // Ball
+            // 
+            this.Ball.BackColor = System.Drawing.Color.Transparent;
+            this.Ball.Image = global::Pong_Game.Properties.Resources.pilka;
+            resources.ApplyResources(this.Ball, "Ball");
+            this.Ball.Name = "Ball";
+            this.Ball.TabStop = false;
+            // 
+            // label1_pause
+            // 
+            resources.ApplyResources(this.label1_pause, "label1_pause");
+            this.label1_pause.BackColor = System.Drawing.Color.Transparent;
+            this.label1_pause.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label1_pause.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.label1_pause.Name = "label1_pause";
+            // 
+            // PongWindow
+            // 
+            resources.ApplyResources(this, "$this");
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(35)))), ((int)(((byte)(35)))), ((int)(((byte)(35)))));
+            this.Controls.Add(this.label1_pause);
+            this.Controls.Add(this.Ball);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.Platform_right);
+            this.Controls.Add(this.Platform_left);
+            this.Controls.Add(this.Right_player_point);
+            this.Controls.Add(this.Left_player_point);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.KeyPreview = true;
+            this.MaximizeBox = false;
+            this.Name = "PongWindow";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.PongWindow_KeyDown);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.PongWindow_KeyUp);
+            ((System.ComponentModel.ISupportInitialize)(this.Platform_left)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Platform_right)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Ball)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox Left_player_point;
+        private System.Windows.Forms.TextBox Right_player_point;
+        private System.Windows.Forms.Timer Timer;
+        private System.Windows.Forms.PictureBox Platform_left;
+        private System.Windows.Forms.PictureBox Platform_right;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox Ball;
+        private System.Windows.Forms.Label label1_pause;
+    }
+}
+
---
+++ Projekt Wprowadzenie do programowania - Marek Bojarski 11529/Pong Game/Pong.cs
@@ -0,0 +1,202 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+// Projekt gry Pong 
+// Marek Bojarski
+// Numer studenta: 11529
+
+namespace Pong_Game
+{
+
+    public partial class PongWindow : Form
+    {
+        int z;
+        int x = 8;
+        int y = 8;
+        int points_left;
+        int points_right = 0;
+        char direction_Right;
+        char direction_Left;
+        bool esc;
+
+        public PongWindow()
+        {
+            InitializeComponent();
+        }
+        private void Pause()
+        {
+
+            if (esc == false)
+            {
+                Timer.Enabled = false;
+                esc = true;
+                label1_pause.Visible = true;
+
+            }
+            else
+            {
+                Timer.Enabled = true;
+                esc = false;
+                label1_pause.Visible = false;
+
+            }
+        }
+
+        static int Losuj_polozenie_pilki()
+        {
+            var generator_polozenia = new Random();
+            return generator_polozenia.Next(30, PongWindow.ActiveForm.Height - 30);
+        }
+        static int Losuj_Odbicie()
+        {
+            var generator_odbicia = new Random();
+            return generator_odbicia.Next(6, 8);
+        }
+
+        private void Add_point()
+        {
+            if (x < 0)
+            {
+                points_right++;
+                Right_player_point.Text = Convert.ToString(points_right);
+                Play_sound("dzwieki/lost.wav");
+            }
+            else
+            {
+                points_left++;
+                Left_player_point.Text = Convert.ToString(points_left);
+                Play_sound("dzwieki/lost.wav");
+            }
+        }
+        private void Play_sound(String path)
+        {     
+            try
+            {
+                System.Media.SoundPlayer player = new System.Media.SoundPlayer();
+                player.SoundLocation = path;
+                player.Load();
+                player.Play();
+            }
+            catch
+            {
+
+            }
+         }
+        private void Timer_Tick(object sender, EventArgs e)
+        {
+            Ball.Left += x;
+            Ball.Top += y;
+
+            // odbicie od platformy lewej
+            if ((Ball.Left <= Platform_left.Left + Platform_left.Width + 5) &&
+                (Ball.Left >= Platform_left.Left + Platform_left.Width) &&
+                (Ball.Top + Ball.Height >= Platform_left.Top) &&
+                (Ball.Top <= Platform_left.Top + Platform_left.Height))
+            {
+                if (x > 0)
+                {
+                    x = -Losuj_Odbicie();
+                }
+                else x = Losuj_Odbicie();
+                Play_sound("dzwieki/hit.wav");
+
+            }
+            // odbicie od platformy prawej
+            if ((Ball.Left + Ball.Width >= Platform_right.Left - 5) &&
+                (Ball.Left + Ball.Width < Platform_right.Left) &&
+                (Ball.Top + Ball.Height >= Platform_right.Top) &&
+                (Ball.Top <= Platform_right.Top + Platform_right.Height))
+            {
+                if (x > 0)
+                {
+                    x = -Losuj_Odbicie();
+                }
+                else x = Losuj_Odbicie();
+
+                Play_sound("dzwieki/hit.wav");
+
+            }
+            // Piłka trafiła
+            if (Ball.Left < 0 || Ball.Left + Ball.Width > PongWindow.ActiveForm.Width)
+            {
+                Ball.Left = 378;
+                Ball.Top = Losuj_polozenie_pilki();
+                x = -x;
+                Add_point();
+            }
+            // odbicie z góry
+            if (Ball.Top < 0) {
+                Ball.Top = 20;
+                if (y > 0)
+                {
+                    y = -Losuj_Odbicie();
+                }
+                else y = Losuj_Odbicie();
+                Play_sound("dzwieki/hit.wav");
+            }
+            // odbicie z dołu
+            if (Ball.Bottom > PongWindow.ActiveForm.Height - 50) {
+                Ball.Top = PongWindow.ActiveForm.Height - 70;
+                if (y > 0)
+                {
+                    y = -Losuj_Odbicie();
+                }
+                else y = Losuj_Odbicie();
+                Play_sound("dzwieki/hit.wav");
+            }
+
+            if (direction_Left == 'W')
+            {
+                if (Platform_left.Top >= 15)
+                {
+                    Platform_left.Top -= 14;
+                }
+            }
+            if (direction_Left == 'S')
+            {
+                if (Platform_left.Top + Platform_left.Height <= PongWindow.ActiveForm.Height - 50)
+                {
+                    Platform_left.Top += 14;
+                }
+            }
+
+            if (direction_Right == 'U')
+            {
+                if (Platform_right.Top >= 15)
+                {
+                    Platform_right.Top -= 14;
+                }
+            }
+            if (direction_Right == 'D')
+            {
+                if (Platform_right.Top + Platform_right.Height <= PongWindow.ActiveForm.Height - 50)
+                {
+                    Platform_right.Top += 14;
+                }
+            }
+        }   
+        private void PongWindow_KeyDown(object sender, KeyEventArgs e)
+        {
+
+            if (e.KeyCode == Keys.W) direction_Left = 'W';
+            if (e.KeyCode == Keys.Up) direction_Right = 'U';
+            if (e.KeyCode == Keys.S) direction_Left = 'S';
+            if (e.KeyCode == Keys.Down) direction_Right = 'D';
+            if (e.KeyCode == Keys.Escape) Pause();
+        }
+        private void PongWindow_KeyUp(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.W) direction_Left = '.';
+            if (e.KeyCode == Keys.Up) direction_Right = '.';
+            if (e.KeyCode == Keys.S) direction_Left = '.';
+            if (e.KeyCode == Keys.Down) direction_Right = '.';
+        }
+    }
+}
